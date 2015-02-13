fd4cbab0031dd56e026b5abd1ea66c62ea963b73
Steven Reinisch <steven.reinisch@teufel.de>
2015-02-13 11:35:08 +0100

    - added helper methods - testFormatPatternValuesOneString still fails - added uncrustify config

--- locela/Locela/format/RFMessageFormatter.m
+++ locela/Locela/format/RFMessageFormatter.m
@@ -35,10 +35,101 @@
     return self;
 }
 
+#pragma mark -
+
 - (NSString *)formatPattern:(NSString *)pattern
                      values:(id)firstValue, ...
 {
+
+    NSArray *values = [self values:firstValue];
+
+    NSError *error = NULL;
+    NSRegularExpressionOptions regexOptions = NSRegularExpressionCaseInsensitive;
+    NSString *regExpPattern = @"\\{\\d\\}";
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:regExpPattern
+                                                                           options:regexOptions
+                                                                             error:&error];
+    if (error)
+    {
+        NSLog(@"Couldn't create regex with given string and options");
+    }
+
+    [regex enumerateMatchesInString:pattern
+                            options:0
+                              range:NSMakeRange(0, pattern.length)
+                         usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+
+         NSString  *placeholder = [pattern substringWithRange:result.range];
+         NSInteger index        = [self indexFromPlaceholder:placeholder];
+         if (0 <= index && index < [values count])
+         {
+             id value = values[index];
+
+         }
+     }];
+
+    //search pattern for matches for {index}
+    //get index from match
+    //replace match with values[index]
+
     return nil;
 }
+
+#pragma mark - private
+
+- (NSArray *)values:(id)firstValue, ...
+{
+    NSMutableArray *values = [[NSMutableArray alloc] init];//get from arg list
+
+    if (!firstValue)
+    {
+        return values;
+    }
+    
+    va_list args;
+    va_start(args, firstValue);
+
+    id value = firstValue;
+    do
+    {
+        [values addObject:value];
+    }
+    while( (value = va_arg(args, id)) );
+
+    va_end(args);
+
+    return values;
+}
+
+- (NSInteger)indexFromPlaceholder:(NSString *)placeholder
+{
+    NSRange   range        = NSMakeRange(1, placeholder.length - 1);
+    NSString  *parsedIndex = [placeholder substringWithRange:range];
+    NSScanner *scanner     = [NSScanner scannerWithString:parsedIndex];
+    int       scannedIndex = -1;
+    BOOL      isInt        = [scanner scanInt:&scannedIndex];
+
+    if (isInt)
+    {
+        return scannedIndex;
+    }
+    else
+    {
+        return NSIntegerMin;
+    }
+}
+
+- (NSString *)replaceValue:(id)value
+                  inString:(NSString *)string
+                   inRange:(NSRange)range
+{
+    NSMutableString *mutable = [NSMutableString stringWithString:string];
+    
+    NSString *valueString = [NSString stringWithFormat:@"%@", value];
+    [mutable replaceCharactersInRange:range withString:valueString];
+    
+    return [NSString stringWithString:mutable];
+}
+
 
 @end
--- locela/locelaTests/RFMessageFormatterTest.m
+++ locela/locelaTests/RFMessageFormatterTest.m
@@ -15,6 +15,12 @@
 
 @property (nonatomic, strong) NSLocale *locale;
 
+- (NSArray *)values:(id)firstValue, ...;
+- (NSInteger)indexFromPlaceholder:(NSString *)placeholder;
+- (NSString *)replaceValue:(id)value
+                  inString:(NSString *)string
+                   inRange:(NSRange)range;
+
 @end
 
 #pragma mark -
@@ -60,5 +66,84 @@
 
     XCTAssertEqualObjects(locale, self.sut.locale, @"locale must match given locale");
 }
+
+#pragma mark - formatPattern:values
+
+- (void)testFormatPatternValuesOneString
+{
+    NSString *result = [self.sut formatPattern:@"Hello {0}!"
+                                        values:@"Klaus", nil];
+
+    XCTAssertEqualObjects(@"Hello Klaus", result);
+}
+
+#pragma mark - values:
+
+- (void)testValuesNoValue
+{
+    NSArray *values = [self.sut values:nil];
+
+    XCTAssertEqual([values count], 0);
+}
+
+- (void)testValuesMultipleValues
+{
+    id value1 = @"Foo";
+    id value2 = @(3);
+
+    NSArray *values = [self.sut values:value1, value2, nil];
+    
+    XCTAssertEqual([values count], 2);
+    XCTAssertEqualObjects(value1, values[0]);
+    XCTAssertEqualObjects(value2, values[1]);
+}
+
+#pragma mark - indexFromPlaceholder:
+
+- (void)testIndexFromPlaceholder
+{
+    NSInteger index = [self.sut indexFromPlaceholder:@"{3}"];
+    XCTAssertEqual(index, 3);
+    
+    index = [self.sut indexFromPlaceholder:@"{-1}"];
+    XCTAssertEqual(index, -1);
+}
+
+- (void)testIndexFromPlaceholderNoIndex
+{
+    NSInteger index = [self.sut indexFromPlaceholder:@"{a}"];
+    XCTAssertEqual(index, NSIntegerMin);
+    
+    index = [self.sut indexFromPlaceholder:@"{}"];
+    XCTAssertEqual(index, NSIntegerMin);
+}
+
+- (void)testIndexFromPlaceholderNil
+{
+    NSInteger index = [self.sut indexFromPlaceholder:nil];
+    XCTAssertEqual(index, NSIntegerMin);
+}
+
+- (void)testIndexFromPlaceholderEmptyString
+{
+    NSInteger index = [self.sut indexFromPlaceholder:@""];
+    XCTAssertEqual(index, NSIntegerMin);
+}
+
+#pragma mark - replaceValue:inString:inRange
+
+- (void)testReplaceValueInStringInRange
+{
+    NSString *string = @"Hello X";
+    NSString *value  = @"Peter";
+    NSRange  range   = NSMakeRange(string.length - 1, 1);
+
+    NSString *result = [self.sut replaceValue:value
+                                     inString:string
+                                      inRange:range];
+
+    XCTAssertEqualObjects(result, @"Hello Peter");
+}
+
 
 @end
