e5de1d38ca30b71162cf0321398bf1e60485cf3d
Steven Reinisch <steven.reinisch@teufel.de>
2015-02-13 08:17:41 +0100

    created static lib project

---
+++ locela/locela/RFLocela.m
@@ -0,0 +1,13 @@
+//
+//  Locela.m
+//  Locela
+//
+//  Created by steven reinisch on 13/02/15.
+//  Copyright (c) 2015 teufel lautsprecher. All rights reserved.
+//
+
+#import "RFLocela.h"
+
+@implementation RFLocela
+
+@end
