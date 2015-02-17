0403b58cb840b3bf3cd19df13721d6c6e7653088
Steven Reinisch <steven.reinisch@teufel.de>
2015-02-17 10:11:47 +0100

    sketeched control flow with empty methods

--- locela/Locela/format/RFNumberFormatter.m
+++ locela/Locela/format/RFNumberFormatter.m
@@ -4,6 +4,7 @@
 //
 
 #import "RFNumberFormatter.h"
+#import "RFMessageFormatter.h"
 
 
 @implementation RFNumberFormatter
@@ -14,7 +15,51 @@
                                      result:(NSString **)result
                                       error:(NSError **)error
 {
-    return NO;
+    NSAssert(string, @"Cannot format nil");
+    NSAssert(match,  @"Cannot format without a match");
+    NSAssert(values, @"Cannot format without values");
+
+    NSString *placeholder  = [string substringWithRange:match.range];
+    NSInteger index        = [self indexFromPlaceholder:placeholder];
+    NSString *numberFormat = [self numberFormatFromPlaceholder:placeholder];
+
+    if (0 <= index && index < [values count])
+    {
+        id value = values[index];
+        *result = [self replaceValue:value
+                            inString:string
+                             inRange:match.range
+                        numberFormat:numberFormat];
+
+        return YES;
+    }
+    else {
+        *error = [NSError errorWithDomain:kRFMessageFormatterErrorDomain
+                                     code:RFMessageFormatterErrorCodeNoValueAtIndex
+                                 userInfo:@{@"index" : @(index)}];
+
+        return NO;
+    }
+}
+
+#pragma mark - private
+
+- (NSInteger)indexFromPlaceholder:(NSString *)placeholder
+{
+    return 0;
+}
+
+- (NSString *)numberFormatFromPlaceholder:(NSString *)placeholder
+{
+    return nil;
+}
+
+- (NSString *)replaceValue:(id)value
+                  inString:(NSString *)string
+                   inRange:(NSRange)range
+              numberFormat:(NSString *)numberFormat
+{
+    return nil;
 }
 
 @end
